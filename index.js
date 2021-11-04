e6a3c26794b9962be48b86849eeceac7b9b4612b
Austin  Marrazza <austinmarrazza@Austins-MacBook-Pro.local>
2021-11-04 15:16:39 -0700

    revised task 7 to access array defined in parameter, not the reviews array directly

--- index.js
+++ index.js
@@ -155,7 +155,7 @@ Use the getLastReview function below to do the following:
 
 
 function getLastReview(array) {
-  return `${reviews[reviews.length -1].name} gave the restaurant a ${reviews[reviews.length -1].rating} star review, and their feedback was: ${reviews[reviews.length -1].feedback}`
+  return `${array[array.length -1].name} gave the restaurant a ${array[array.length -1].rating} star review, and their feedback was: ${array[array.length -1].feedback}`
 } 
 
 console.log('task 7', getLastReview(reviews));
