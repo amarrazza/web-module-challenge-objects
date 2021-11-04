7c2b8bd3a6abdd8495072bbb28fcef4f6e24a2fc
Austin  Marrazza <austinmarrazza@Austins-MacBook-Pro.local>
2021-11-04 14:36:49 -0700

    finished tasks 1-7

--- index.js
+++ index.js
@@ -154,11 +154,11 @@ Use the getLastReview function below to do the following:
 */
 
 
-function getLastReview(/*Your code here*/) {
-  /*Your code here*/
+function getLastReview(array) {
+  return `${reviews[reviews.length -1].name} gave the restaurant a ${reviews[reviews.length -1].rating} star review, and their feedback was: ${reviews[reviews.length -1].feedback}`
 } 
 
-
+console.log('task 7', getLastReview(reviews));
 
 ///////////////🍔☕️🍽 STRETCH🍔☕️🍽////////////////////
 
